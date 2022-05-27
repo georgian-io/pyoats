558ee015831d8bcd868bcdf923a1f26a64d56570
Benjamin Ye <benjamin.ye@georgian.io>
2022-05-27 22:13:57 +0000

    changing back default # of runs

--- predictive_exp.py
+++ predictive_exp.py
@@ -23,12 +23,12 @@ def run_model(m, data, fdir):
     test_data, test_label = data.test
 
     print("Tuning window and step sizes")
-    m.hyperopt_ws(train_data, test_data, 8)
+    m.hyperopt_ws(train_data, test_data, 8 * 8)
 
     model_test_data, model_test_label = data.get_test_with_window(m.window)
 
     print("Tuning model hyperparameters")
-    m.hyperopt_model(train_data, model_test_data, 8)
+    m.hyperopt_model(train_data, model_test_data, 8 * 8)
     m.fit(train_data)
 
     print("Generating predictions and scores")
@@ -49,11 +49,9 @@ def main():
         data = reader(ROOT_DIR + file)
         fdir = f"{SAVE_DIR}{data_name}/"
         
-        """
         for model in SIMPLE_MODELS:
             m = model()
             run_model(m, data, fdir)
-        """
 
         for model in DL_MODELS:
             m = model(use_gpu=True, val_split=0.1)
