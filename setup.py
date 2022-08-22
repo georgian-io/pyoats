7147bcca796a4a6c13ac2e4f746dbc334cb3170a
benjaminye <benjamin.ye@georgian.io>
2022-08-22 17:21:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='one-ts',
+setup(name='one',
       version='1.0',
       description='Outlier Number Extraction for TimeSeries',
       author='Benjamin Ye',
